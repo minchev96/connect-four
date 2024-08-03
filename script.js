6cb92e085fe84b2a076086e7d34bb33f4ad50a31
minchev96 <monislavminchev@gmail.com>
2024-08-03 09:55:32 +0300

    feat: added drop token functionality

--- script.js
+++ script.js
@@ -12,8 +12,19 @@ function Gameboard(){
 
     const getBoard = () => board;
 
-    return { getBoard }
+    const dropToken = function(column, player) {
+        const availablePositions = board.filter(row => row[column] === 0).map(row => row[column]);
+        if(!availablePositions.length) return;
+
+        const lowestRow = availablePositions.length - 1;
+        board[lowestRow][column] = player;
+    }
+
+    return { getBoard, dropToken}
 }
 
 const board = Gameboard();
+board.dropToken(0, 1);
+board.dropToken(1, 1);
+
 console.log(board.getBoard());
