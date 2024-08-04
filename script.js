7f6c2763032e17a5b727fd347da92b3b3b91e3f7
minchev96 <monislavminchev@gmail.com>
2024-08-04 10:14:49 +0300

    feat: added players turn and vertical win logic

--- script.js
+++ script.js
@@ -23,8 +23,60 @@ function Gameboard(){
     return { getBoard, dropToken}
 }
 
+function playersTurn(){
+    const players = [
+        {
+            name: "playerOne",
+            token: 1
+        },
+        {
+            name: "playerTwo",
+            token: 2
+        }
+    ]
+    let activePlayer = players[0];
+    const getActivePlayer = () => activePlayer;
+
+    const switchPlayer = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+        console.info(`${activePlayer.name} turn`)
+    }
+
+    return { getActivePlayer, switchPlayer};
+}
+
+function checkWinner(){
+    const winnerMatches = 3;
+    const test = board.getBoard();
+    let matches = 0;
+    let hasWinner = false;
+    console.error(board.getBoard());
+    //vertical win check
+    for(let i = 0; i < test.length + 1; i++){
+        if(hasWinner) break;
+        for(let j = 0; j < test.length; j++){
+            if(test[j][i] === 1){
+                matches++;
+            }
+            else{
+                matches = 0;
+            }
+            if(matches === winnerMatches) {
+                console.log('win');
+                hasWinner = true;
+                break;
+            }
+        }
+    }
+}
+
 const board = Gameboard();
-board.dropToken(0, 1);
-board.dropToken(1, 1);
+const player = playersTurn();
+board.dropToken(0, player.getActivePlayer().token);
+player.switchPlayer();
+board.dropToken(1, player.getActivePlayer().token);
+player.switchPlayer();
 
 console.log(board.getBoard());
+
+checkWinner();
